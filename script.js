9bf4f5ffeaaf13aced6c1def38e2f2389dd88c5c
guudewie <philippthiessen12@gmail.com>
2023-07-03 14:48:55 +0200

    add Book contructor

---
+++ script.js
@@ -0,0 +1,25 @@
+// event listener create book entry
+// event listener add book to library
+// event listener delete book
+
+
+let myLibrary = [];
+
+
+
+// the constructor...
+function Book(title, author, pages, read) {
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+}
+
+
+
+// do stuff here
+function addBookToLibrary() {
+
+}
