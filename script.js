ea369f37d3d885b4cebf48ad1ceb36d5938d9336
guudewie <philippthiessen12@gmail.com>
2023-07-06 10:26:22 +0200

    add modal layout and js logic

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-// event listener create book entry
-// event listener add book to library
+/****************** TO DO *************************/
+
 // event listener delete book
+// display books in array
+// add book to array
+
+
+
+
+//query selectors
+OPEN_MODAL_BTN = document.getElementById("add-book")
+MODAL = document.getElementById("modal")
+ADD_BOOK = document.getElementById("form-submit")
+
+MYFORM = document.getElementById("myform") //form start
+NAME = document.getElementById("name")
+AUTHOR = document.getElementById("author")
+PAGES = document.getElementById("pages")
+READ = document.getElementById("read")
+
+
+//event listener
+OPEN_MODAL_BTN.onclick = function () {
+    MODAL.style.display = "flex";
+}
+
+window.onclick = function(event) {
+    if (event.target == MODAL) {
+      MODAL.style.display = "none";
+    }
+}
+
+MYFORM.addEventListener("submit", function(event) {
+
+    let newBook = new Book(NAME.value, AUTHOR.value, PAGES.value, READ.value);
+    closeModalAndResetForm();
+    event.preventDefault();
+
+})
+
+
+function closeModalAndResetForm() {
+    MYFORM.reset()
+    MODAL.style.display = "none";
+    
+};
 
 
 let myLibrary = [];
@@ -8,13 +51,13 @@ let myLibrary = [];
 
 
 // the constructor...
-function Book(title, author, pages, read) {
+function Book(name, author, pages, read) {
 
-    this.title = title;
+    this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    
+
 }
 
 
