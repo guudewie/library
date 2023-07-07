d8de5bfedb6224a98fe45be574db11c69646c9a4
guudewie <philippthiessen12@gmail.com>
2023-07-07 14:37:27 +0200

    add functionality to change read status on card

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/****************** TO DO *************************/
-
-// 
-
-
-
 //query selectors
 OPEN_MODAL_BTN = document.getElementById("add-book")
 MODAL = document.getElementById("modal")
@@ -59,45 +53,74 @@ function Book(name, author, pages, read) {
 
 function addBookToLibrary(book) {
 
+    let readButton;
+    
+    // use different class for button if book is read or not
+    if (book.read) {
+        readButton = `<button class="read-notread read">Read</button>`;
+    } else {
+        readButton = `<button class="read-notread">Not Read</button>`;
+    }
+
     BOOK_GRID.insertAdjacentHTML(
         "beforeend",
         `<div class="book">
             <span class="delete-card material-symbols-outlined">close</span>
             <div class="container-card">
                 <div class="label title">Title</div>
-                <div id="title-card"">${book.name}</div>
+                <div id="title-card">${book.name}</div>
+            </div>
             <div class="container-card">
                 <div class="label author">Author</div>
                 <div id="author-card">${book.author}</div>
+            </div>
             <div class="container-card">
                 <div class="label pages">Pages</div>
                 <div id="pages-card">${book.pages}</div>
+            </div>
             <div class="container-card">
-                <div class="label read">Read</div>
-                <div id="read-card">${book.read}</div>
+                ${readButton}
+            </div>
         </div>`
     );
 
     let index = myLibrary.length-1;
 
-    //associate DOM element (book) with actual book object via data-index attribute
+    //associate DOM elements with actual book object via data-index attribute
     let recentBook = BOOK_GRID.lastChild;
     recentBook.setAttribute("data-index",index)
-    console.log(recentBook)
 
-    //associate DOM element (close icon) with actual book object via data-index attribute
-    let IconOfRecentBook = document.querySelector("#main :last-child > span")
-    IconOfRecentBook.setAttribute("data-index", index)
+    let iconOfRecentBook = document.querySelector("#main :last-child > span")
+    iconOfRecentBook.setAttribute("data-index", index)
+
+    let readToggleOfRecentBook = document.querySelector("#main :last-child :last-child > button")
+    readToggleOfRecentBook.setAttribute("data-index", index)
+    console.log(readToggleOfRecentBook)
+
 
     // add event listener to delete respective book 
-    IconOfRecentBook.addEventListener("click", () => {
+    iconOfRecentBook.addEventListener("click", () => {
 
-        let index = IconOfRecentBook.getAttribute("data-index");
+        let index = iconOfRecentBook.getAttribute("data-index");
 
         let associatedCard = document.querySelectorAll(`.book[data-index="${index}"]`)
         associatedCard.forEach( (element) => element.remove())
-    
     });
+
+    // add event listener to change read status
+    readToggleOfRecentBook.addEventListener("click", () => {
+
+        readToggleOfRecentBook.classList.toggle("read")
+
+        if (readToggleOfRecentBook.textContent == "Read") {
+            
+            readToggleOfRecentBook.textContent = "Not Read"
+        } else {
+            readToggleOfRecentBook.textContent = "Read"
+        };
+
+    })
+
 };
 
 
